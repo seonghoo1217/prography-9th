97b8a385ba54598dc62d8211028847ea5f121ea3
Lee.t.c <lsh6451217@gmail.com>
2024-02-02 17:07:39 +0900

    refactor: DTO record 타입 변경

--- pingpong/src/main/java/assignment/pingpong/presentation/dto/response/UserPageRes.java
+++ pingpong/src/main/java/assignment/pingpong/presentation/dto/response/UserPageRes.java
@@ -1,19 +1,10 @@
 package assignment.pingpong.presentation.dto.response;
 
 import assignment.pingpong.domain.user.User;
-import lombok.Getter;
 
 import java.util.List;
 
-@Getter
-public class UserPageRes {
-    private long totalElements;
-    private int totalPages;
-    private List<User> userList;
+public record UserPageRes(long totalElements, int totalPages, List<User> userList) {
 
-    public UserPageRes(long totalElements, int totalPages, List<User> userList) {
-        this.totalElements = totalElements;
-        this.totalPages = totalPages;
-        this.userList = userList;
-    }
 }
+
