7dbac043bdd3f4c927e4e750ca8a12819b86950a
Lee.t.c <lsh6451217@gmail.com>
2024-02-02 16:01:00 +0900

    refactor: API Response 양식 클래스타입 변경

--- pingpong/src/main/java/assignment/pingpong/global/dto/ApiResponse.java
+++ pingpong/src/main/java/assignment/pingpong/global/dto/ApiResponse.java
@@ -1,13 +1,57 @@
 package assignment.pingpong.global.dto;
 
-public record ApiResponse<T>(Integer code, String message, T result) {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
 
+import java.util.Objects;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public final class ApiResponse<T> {
+    private final Integer code;
+    private final String message;
+    private T result;
+
+    public ApiResponse(Integer code, String message, T result) {
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+
+    public ApiResponse(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 
     public static <T> ApiResponse<T> of(Integer code, String message, T result) {
         return new ApiResponse<>(code, message, result);
     }
 
     public static <T> ApiResponse<T> of(Integer code, String message) {
-        return new ApiResponse<>(code, message, null);
+        return new ApiResponse<>(code, message);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ApiResponse) obj;
+        return Objects.equals(this.code, that.code) &&
+                Objects.equals(this.message, that.message) &&
+                Objects.equals(this.result, that.result);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, message, result);
+    }
+
+    @Override
+    public String toString() {
+        return "ApiResponse[" +
+                "code=" + code + ", " +
+                "message=" + message + ", " +
+                "result=" + result + ']';
+    }
+
 }
