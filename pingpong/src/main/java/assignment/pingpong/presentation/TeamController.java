35914e0918833ed5373bf94b2e864b67556d56dd
Lee.t.c <lsh6451217@gmail.com>
2024-02-02 22:55:23 +0900

    feat: swagger 주석 추가

--- pingpong/src/main/java/assignment/pingpong/presentation/TeamController.java
+++ pingpong/src/main/java/assignment/pingpong/presentation/TeamController.java
@@ -3,6 +3,8 @@ package assignment.pingpong.presentation;
 import assignment.pingpong.application.room.RoomCommandService;
 import assignment.pingpong.global.dto.ApiResponse;
 import assignment.pingpong.presentation.dto.request.ModifyTeamReq;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,11 @@ public class TeamController {
     private final RoomCommandService roomCommandService;
 
     @PutMapping("/{roomId}")
+    @Operation(summary = "Team 정보 수정")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "유저 ID 또는 방 ID가 없음"),
+    })
     public ApiResponse<?> modifyTeam(@PathVariable Integer roomId, @RequestBody ModifyTeamReq modifyTeamReq) {
         return roomCommandService.modifyTeam(roomId, modifyTeamReq.userId());
     }
--- pingpong/src/main/java/assignment/pingpong/presentation/UserController.java
+++ pingpong/src/main/java/assignment/pingpong/presentation/UserController.java
@@ -4,6 +4,8 @@ import assignment.pingpong.application.user.UserQueryService;
 import assignment.pingpong.global.dto.ApiResponse;
 import assignment.pingpong.presentation.dto.request.PageReq;
 import assignment.pingpong.presentation.dto.response.user.UserPageRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +19,9 @@ public class UserController {
     private final UserQueryService userQueryService;
 
     @GetMapping
-    public ApiResponse<?> findAllUserByPage(@Valid PageReq pageReq) {
+    @Operation(summary = "유저 전체 정보 조회 Page")
+    public ApiResponse<?> findAllUserByPage(@Parameter(name = "size, page", description = "페이징처리를 위한 size, page값")
+                                            @Valid PageReq pageReq) {
         UserPageRes userPageRes = userQueryService.findAllUserByPage(pageReq);
 
         return ApiResponse.of(200, "API 요청이 성공했습니다.", userPageRes);
